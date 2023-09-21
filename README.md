7eebe243de159f423f7aa46f467555acc83ac540
Ron Jeffries <ronjeffries@acm.org>
2023-09-21 14:32:06 -0400

    Added README.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Python Decorator Tutorial
+
+This project includes the tutorial tests from my article [P-264 - Decorator Tutorial](https://ronjeffries.com/articles/-y023/python/-9z260/264/).
+
+Also included are the tests that I wrote while learning how to create the decorators that I use in the tutorial.
+
+Feedback on the tutorial is welcome. Email or Mastodon is best, I tend not to see pull requests.
+
+Enjoy!
--- tests/test_for_article.py
+++ tests/test_for_article.py
@@ -1,3 +1,5 @@
+import functools
+
 import pytest
 
 
@@ -33,4 +35,106 @@ class TestForArticle:
         assert wrapper(walking) == "you are walking, sir!"
         assert wrapper(running) == "you are running, sir!"
         assert wrapper(mumbling) == "you are mumbling, sir!"
+
+    def test_do_twice(self):
+        def do_twice(func):
+            @functools.wraps(func)
+            def wrapper(*args, **kwargs):
+                result = "do_twice begin\n"
+                result += func(*args, **kwargs)
+                result += func(*args, **kwargs)
+                result += "end do_twice\n"
+                return result
+            return wrapper
+
+        @do_twice
+        def hello(name):
+            return "hello, " + name + "!\n"
+
+        said_twice = hello("Avery")
+        assert (said_twice ==
+"""do_twice begin
+hello, Avery!
+hello, Avery!
+end do_twice
+""")
+
+    def test_repeat_parameterized(self):
+        def do_n_times(number_of_times):
+            def get_function(func):
+                @functools.wraps(func)
+                def wrapper(*args, **kwargs):
+                    result = "do_n_times begin\n"
+                    for _ in range(number_of_times):
+                        result += func(*args, **kwargs)
+                    result += "end do_n_times\n"
+                    return result
+                return wrapper
+            return get_function
+
+        @do_n_times(3)
+        def hello(name):
+            return "hello, " + name + "!\n"
+
+        said_twice = hello("Parker")
+        assert (said_twice ==
+"""do_n_times begin
+hello, Parker!
+hello, Parker!
+hello, Parker!
+end do_n_times
+""")
+
+    def test_do_twice_class(self):
+        class do_twice:
+            def __init__(self, func):
+                self.func = func
+
+            def __call__(self, *args, **kwargs):
+                result = "do_twice begin\n"
+                result += self.func(*args, **kwargs)
+                result += self.func(*args, **kwargs)
+                result += "end do_twice\n"
+                return result
+
+        @do_twice
+        def hello(name):
+            return "hello, " + name + "!\n"
+
+        said_twice = hello("Avery")
+        assert (said_twice ==
+"""do_twice begin
+hello, Avery!
+hello, Avery!
+end do_twice
+""")
+
+    def test_do_n_times_class(self):
+        class do_n_times:
+            def __init__(self, number_of_times):
+                self.number_of_times = number_of_times
+
+            def __call__(self, func):
+                self.func = func
+                return self.perform_behavior
+
+            def perform_behavior(self, *args, **kwargs):
+                result = "do_n_times begin\n"
+                for _ in range(self.number_of_times):
+                    result += self.func(*args, **kwargs)
+                result += "end do_n_times\n"
+                return result
+
+        @do_n_times(3)
+        def hello(name):
+            return "hello, " + name + "!\n"
+
+        said_twice = hello("Parker")
+        assert (said_twice ==
+"""do_n_times begin
+hello, Parker!
+hello, Parker!
+hello, Parker!
+end do_n_times
+""")
 
